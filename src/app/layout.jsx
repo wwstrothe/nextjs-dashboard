6a215b39edecd522d53b3b4fc298bf915e9ee9ce
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2026-09-10 07:49:07 -0700

    nextjs dashboard source code

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,17 +1,16 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { inter } from '@/app/ui/fonts';
+import '@/app/ui/global.css'; // Import global styles
 
 export const metadata = {
-  title: "MO-PCCO.R3 - NextJS Financial Dashboard",
-  description: "Starter code for Module 25 Capstone project - Financial Dashbard",
+  title: 'MO-PCCO.R3 - NextJS Financial Dashboard',
+  description:
+    'Starter code for Module 25 Capstone project - Financial Dashbard',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
--- src/app/ui/invoices/status.jsx
+++ src/app/ui/invoices/status.jsx
@@ -9,6 +9,7 @@ export default function InvoiceStatus({ status }) {
         {
           'bg-gray-100 text-gray-500': status === 'pending',
           'bg-green-500 text-white': status === 'paid',
+          'bg-red-500 text-white': status === 'late',
         },
       )}
     >
@@ -22,6 +23,12 @@ export default function InvoiceStatus({ status }) {
         <>
           Paid
           <CheckIcon className="ml-1 w-4 text-white" />
+        </>
+      ) : null}
+      {status === 'late' ? (
+        <>
+          Late
+          <ClockIcon className="ml-1 w-4 text-white" />
         </>
       ) : null}
     </span>
