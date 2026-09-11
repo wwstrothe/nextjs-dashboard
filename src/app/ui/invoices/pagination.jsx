6d0555a760c470021107138ad48850f495747389
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2026-09-11 09:17:06 -0700

    Enhance Invoices Page with Search, Pagination, and Improved Structure

--- src/app/ui/invoices/pagination.jsx
+++ src/app/ui/invoices/pagination.jsx
@@ -1,20 +1,28 @@
 'use client';
 
+import { generatePagination } from '@/app/lib/utils';
 import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
-import { generatePagination } from '@/app/lib/utils';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Pagination({ totalPages }) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const currentPage = Number(searchParams.get('page')) || 1;
   // NOTE: comment in this code when you get to this point in the course
 
-  // const allPages = generatePagination(currentPage, totalPages);
+  const allPages = generatePagination(currentPage, totalPages);
+
+  const createPageURL = (pageNumber) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', pageNumber.toString());
+    return `${pathname}?${params.toString()}`;
+  };
 
   return (
     <>
-      {/* NOTE: comment in this code when you get to this point in the course */}
-
-      {/* <div className="inline-flex">
+      <div className="inline-flex">
         <PaginationArrow
           direction="left"
           href={createPageURL(currentPage - 1)}
@@ -23,7 +31,7 @@ export default function Pagination({ totalPages }) {
 
         <div className="flex -space-x-px">
           {allPages.map((page, index) => {
-            let position: 'first' | 'last' | 'single' | 'middle' | undefined;
+            let position; // 'first' | 'last' | 'single' | 'middle' | undefined;
 
             if (index === 0) position = 'first';
             if (index === allPages.length - 1) position = 'last';
@@ -47,17 +55,12 @@ export default function Pagination({ totalPages }) {
           href={createPageURL(currentPage + 1)}
           isDisabled={currentPage >= totalPages}
         />
-      </div> */}
+      </div>
     </>
   );
 }
 
-function PaginationNumber({
-  page,
-  href,
-  isActive,
-  position,
-}) {
+function PaginationNumber({ page, href, isActive, position }) {
   const className = clsx(
     'flex h-10 w-10 items-center justify-center text-sm border',
     {
@@ -78,11 +81,7 @@ function PaginationNumber({
   );
 }
 
-function PaginationArrow({
-  href,
-  direction,
-  isDisabled,
-}) {
+function PaginationArrow({ href, direction, isDisabled }) {
   const className = clsx(
     'flex h-10 w-10 items-center justify-center rounded-md border',
     {
